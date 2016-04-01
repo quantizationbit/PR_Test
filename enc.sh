d73fcde0be9a43697acbaabccf1d1ff19f4349d3
Bill Mandel <quantizationbit@gmail.com>
2016-04-01 14:05:38 -0700

    simple encode

--- enc.sh
+++ enc.sh
@@ -11,7 +11,6 @@ function encodeUHD2020PQ {
 x265 --input $YUVFILE --input-depth 10 --input-res 3840x2160 --fps 23.98 \
   --profile main10 --level-idc 51 --no-high-tier --tune grain \
   --crf $crf --aq-mode 3  \
-  --frames 120 \
    -p slow  --bframes 12 -I 72 --sar 1 --range limited \
   --colorprim bt2020 --transfer 16 --colormatrix bt2020nc --chromaloc 2 \
   --master-display "G(13250,34500)B(7500,3000)R(34000,16000)WP(15635,16450)L(10000000,50)" \
@@ -20,6 +19,8 @@ x265 --input $YUVFILE --input-depth 10 --input-res 3840x2160 --fps 23.98 \
   -o x265-$YUVBASE-$crf.bin 2>&1 | tee logencoder-x265-$YUVBASE-$crf.txt
 	
 }
+
+#   --frames 120 \
 
 
 
@@ -33,7 +34,7 @@ prores="Lr_5minClip_HDR_UHD2160_240_ProRes4444_rec2020_PQ_1000nits_2398p_Clipste
 
 
 ## Audio
-#ffmpeg -y -i $prores -vn -ac 6 -acodec libfdk_aac -cutoff 18000 -ab 768k audio.aac
+ffmpeg -y -i $prores -vn -ac 6 -acodec libfdk_aac -cutoff 18000 -ab 768k audio.aac
 
 
 
--- yuv.sh
+++ yuv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -x
 
 #ffmpeg -y -i Lr_5minClip_HDR_UHD2160_240_ProRes4444_rec2020_PQ_1000nits_2398p_Clipster6Test.mov \
    #-an  -vcodec tiff tif/%06d.tif
@@ -8,15 +9,72 @@
 # Create YUV
 # Build YUV
 rm -fv YUV.yuv
+#for filename in tif/*.tif ; do
+
+#ctlrender -force -ctl $EDRHOME/ACES/CTLa1/Full-2-VideoRange.ctl \
+ #$filename /dev/shm/temp.tif
+#$EDRHOME/Tools/YUV/tif2yuv /dev/shm/temp.tif B10 2020  -o YUV.yuv
+
+#done
+
+#rm -fv /dev/shm/temp.tif
+   
+
+
+
+# find all exr files
+c1=0
+CMax=7
+
+
 for filename in tif/*.tif ; do
 
+ 
+if [ $c1 -le $CMax ]; then
+
 ctlrender -force -ctl $EDRHOME/ACES/CTLa1/Full-2-VideoRange.ctl \
- $filename /dev/shm/temp.tif
-$EDRHOME/Tools/YUV/tif2yuv /dev/shm/temp.tif B10 2020  -o YUV.yuv
+ $filename /dev/shm/temp$c1".tif" &
+
+
+
+c1=$[$c1 +1]
+fi
+
+if [ $c1 -gt $CMax ]; then
+for job in `jobs -p`
+do
+echo $job
+wait $job 
+done
+c1=0
+# put in YUV frames
+for i in $(seq 0 $CMax); do
+$EDRHOME/Tools/YUV/tif2yuv /dev/shm/temp$i".tif" B10 2020  -o YUV.yuv
+done
+
+fi
 
 
 
 done
 
-rm -fv /dev/shm/temp.tif
-   
+
+# make sure all jobs finished
+for job in `jobs -p`
+do
+echo $job
+wait $job 
+done
+
+
+# put in YUV frames
+for i in {0..$c1}; do
+$EDRHOME/Tools/YUV/tif2yuv /dev/shm/temp$i".tif" B10 2020  -o YUV.yuv
+done
+
+
+
+# cleanup
+rm -fv /dev/shm/temp*tif
+
+
