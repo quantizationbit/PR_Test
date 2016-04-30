17a2aa42ae2341bd2d9e4319e0a2fd57cd030750
Bill Mandel <quantizationbit@gmail.com>
2016-04-30 08:58:00 -0700

    cleanup

--- yuvcompare.sh
+++ yuvcompare.sh
@@ -25,7 +25,7 @@ ffmpeg -y -i ../$prores \
 
    
 # assume prores is 2020 yuv 444 matrix
-$EDRHOME/Tools/YUV/yuv2tif Direct.yuv B10 2020
+$EDRHOME/Tools/YUV/yuv2tif Direct.yuv B10 2020C
 rm -rfv tif2020
 mv tifXYZ tif2020
 ctlrender -force -ctl $EDRHOME/ACES/CTL/null.ctl \
@@ -88,6 +88,7 @@ done
         
 filename="sc2020yuv-"$run".log"
 $EDRHOME/Tools/demos/sc/sigma_compare_PQ Frame834.exr F834_2020yuv.exr | tee $filename
+rm -fv *data
 
 
 sed -n -e /"PQ Ave"/p -e /"#10k16b-"/p $filename | sed -n -e /sigma_red/p -e /"#10k16b-"/p | sed s/"#10k16b-".*//g | sed -e s/"pixels, PQ Ave".*//g | sed -e s/"sigma_red".// | sed -e s/"] ="// | sed -e s/"self_relative =".*"(".*" ="// | sed s/" for"// | sed s/"pixels".*//  | tee X.data
@@ -108,6 +109,7 @@ convert -alpha off -density 300 \
     
 filename="sc709yuv-"$run".log"
 $EDRHOME/Tools/demos/sc/sigma_compare_PQ Frame834.exr F834_709yuv.exr | tee $filename
+rm -fv *data
 
 
 sed -n -e /"PQ Ave"/p -e /"#10k16b-"/p $filename | sed -n -e /sigma_red/p -e /"#10k16b-"/p | sed s/"#10k16b-".*//g | sed -e s/"pixels, PQ Ave".*//g | sed -e s/"sigma_red".// | sed -e s/"] ="// | sed -e s/"self_relative =".*"(".*" ="// | sed s/" for"// | sed s/"pixels".*//  | tee X.data
@@ -128,6 +130,7 @@ convert -alpha off -density 300 \
 
 filename="scffmpeg-"$run".log"
 $EDRHOME/Tools/demos/sc/sigma_compare_PQ Frame834.exr F834_ffmpeg.exr | tee $filename
+rm -fv *data
 
 
 sed -n -e /"PQ Ave"/p -e /"#10k16b-"/p $filename | sed -n -e /sigma_red/p -e /"#10k16b-"/p | sed s/"#10k16b-".*//g | sed -e s/"pixels, PQ Ave".*//g | sed -e s/"sigma_red".// | sed -e s/"] ="// | sed -e s/"self_relative =".*"(".*" ="// | sed s/" for"// | sed s/"pixels".*//  | tee X.data
@@ -150,6 +153,7 @@ convert -alpha off -density 300 \
 
 filename="sc10bit.log"
 $EDRHOME/Tools/demos/sc/sigma_compare_PQ Frame834.exr F834_10bit.exr | tee $filename
+rm -fv *data
 
 
 sed -n -e /"PQ Ave"/p -e /"#10k16b-"/p $filename | sed -n -e /sigma_red/p -e /"#10k16b-"/p | sed s/"#10k16b-".*//g | sed -e s/"pixels, PQ Ave".*//g | sed -e s/"sigma_red".// | sed -e s/"] ="// | sed -e s/"self_relative =".*"(".*" ="// | sed s/" for"// | sed s/"pixels".*//  | tee X.data
@@ -171,6 +175,7 @@ convert -alpha off -density 300 \
 
 filename="sc12bit.log"
 $EDRHOME/Tools/demos/sc/sigma_compare_PQ Frame834.exr F834_12bit.exr | tee $filename
+rm -fv *data
 
 
 sed -n -e /"PQ Ave"/p -e /"#10k16b-"/p $filename | sed -n -e /sigma_red/p -e /"#10k16b-"/p | sed s/"#10k16b-".*//g | sed -e s/"pixels, PQ Ave".*//g | sed -e s/"sigma_red".// | sed -e s/"] ="// | sed -e s/"self_relative =".*"(".*" ="// | sed s/" for"// | sed s/"pixels".*//  | tee X.data
@@ -200,22 +205,23 @@ proresSwitch="LS_5frames_HDR_rec2020_PQ_1000nit_ProRes4444XQ_24p_Switch162_Video
 proresFA="LS_10frames_HDR_rec2020_PQ_1000nit_ProRes4444XQ_24p_FlameAssist2106_VideoOnlyTest.mov"
 f834="LS_R3_3840x2160_24Fps_16bit_rec2020_PQ_FullRange_1000nit_Master.0260226.tiff"
 
+
+prores=$proresFA
+run="FA"
+FRAMETEST
+
+
 prores=$proresGIC
 run="GIC"
-
 FRAMETEST
 
 
 prores=$proresSwitch
 run="Switch"
-
 FRAMETEST
 
 
-prores=$proresFA
-run="FA"
 
-FRAMETEST
 
 
 
