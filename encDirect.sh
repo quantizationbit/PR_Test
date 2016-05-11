c01fcdd23f4265a4e6351afd101a6791072a9267
Bill Mandel <quantizationbit@gmail.com>
2016-05-11 03:57:46 -0700

    direct encode

--- encDirect.sh
+++ encDirect.sh
@@ -2,13 +2,13 @@
 
 set -x
 
-YUVBASE="Direct1000"
+YUVBASE="Direct1000CLI"
 
 
 # Encode
 crf=12
 
-(ffmpeg -y -i Lr_5minClip_HDR_UHD2160_240_ProRes4444_rec2020_PQ_1000nits_2398p_Clipster6Test.mov \
+(ffmpeg -y -i LS_5minClip_UHD_3840x2160_rec2020_PQ_1000nit_ProRes4444_24p_AppleCLI_VideoOnlyTest.mov \
    -an  -pix_fmt yuv420p10le -f rawvideo -vcodec rawvideo - | x265 --input - \
    --input-depth 10 --input-res 3840x2160 --fps 23.98 \
    --profile main10 --level-idc 51 --no-high-tier --tune grain \
@@ -30,11 +30,12 @@ prores="Lr_5minClip_HDR_UHD2160_240_ProRes4444_rec2020_PQ_1000nits_2398p_Clipste
 
 
 ## Audio
-ffmpeg -y -i $prores -vn   \
-       -filter_complex "[0:2][0:3][0:4][0:5][0:6][0:7]amerge=inputs=6[aout]" \
-       -map "[aout]" \
-       -ac 6  -acodec libfdk_aac -cutoff 18000 -ab 768k audio.aac
+#ffmpeg -y -i $prores -vn   \
+       #-filter_complex "[0:2][0:3][0:4][0:5][0:6][0:7]amerge=inputs=6[aout]" \
+       #-map "[aout]" \
+       #-ac 6  -acodec libfdk_aac -cutoff 18000 -ab 768k audio.aac
 
+ffmpeg -y -i LS_5minClip_51D_Audio_24p_48kHz_24bit.wav -acodec libfdk_aac -cutoff 18000 -ab 768k audio.aac
 
 
 
